7ae38973ad15837f33d321326cbf1556c31a530b
Josh Hill <jamesjoshuahill@gmail.com>
2013-11-20 16:36:17 +0000

    Make numbers_in_words hash a constant and make code consistent

--- one_million.rb
+++ one_million.rb
@@ -1,44 +1,45 @@
 class Fixnum
+  
+  NUMBERS_IN_WORDS = {
+    1  => "one",
+    2  => "two",
+    3  => "three",
+    4  => "four",
+    5  => "five",
+    6  => "six",
+    7  => "seven",
+    8  => "eight",
+    9  => "nine",
+    10 => "ten",
+    11 => "eleven",
+    12 => "twelve",
+    13 => "thirteen",
+    14 => "fourteen",
+    15 => "fifteen",
+    16 => "sixteen",
+    17 => "seventeen",
+    18 => "eighteen",
+    19 => "nineteen",
+    20 => "twenty",
+    30 => "thirty",
+    40 => "forty",
+    50 => "fifty",
+    60 => "sixty",
+    70 => "seventy",
+    80 => "eighty",
+    90 => "ninety"
+  }
 
   def in_words
-    numbers_in_words =  {
-      1       => "one",
-      2       => "two",
-      3       => "three",
-      4       => "four",
-      5       => "five",
-      6       => "six",
-      7       => "seven",
-      8       => "eight",
-      9       => "nine",
-      10      => "ten",
-      11      => "eleven",
-      12      => "twelve",
-      13      => "thirteen",
-      14      => "fourteen",
-      15      => "fifteen",
-      16      => "sixteen",
-      17      => "seventeen",
-      18      => "eighteen",
-      19      => "nineteen",
-      20      => "twenty",
-      30      => "thirty",
-      40      => "forty",
-      50      => "fifty",
-      60      => "sixty",
-      70      => "seventy",
-      80      => "eighty",
-      90      => "ninety"
-    }
     if self < 20
-      numbers_in_words[self]
+      NUMBERS_IN_WORDS[self]
     elsif self.multiple_of?(10) && self < 100
-      numbers_in_words[self]
+      NUMBERS_IN_WORDS[self]
     elsif self < 100
-      ones_digit = self % 10
-      tens_digit = self - ones_digit
+      remainder = self % 10
+      tens_digit = self - remainder
       
-      [tens_digit, ones_digit].map(&:in_words).join(' ')
+      tens_digit.in_words + " " + ones_digit.in_words
     elsif self.multiple_of?(100) && self < 1000
       hundreds_digit = self / 100
 
